b47201684ab88d771c01103c87134423681772ed
Zuhair Qureshi <zuhair.q01@gmail.com>
2025-11-09 13:49:01 -0500

    Map with markers working

--- backend/scripts/fetch_firms_data.py
+++ backend/scripts/fetch_firms_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # URL of the FIRMS API for VIIRS data
 def fetch_firms_data():
-    url = "https://firms.modaps.eosdis.nasa.gov/api/area/csv/959239e98f8de0554b6c5be997d5c4ec/VIIRS_SNPP_NRT/L/1"
+    url = "https://firms.modaps.eosdis.nasa.gov/api/area/csv/959239e98f8de0554b6c5be997d5c4ec/VIIRS_SNPP_NRT/world/1"
 
     # Fetch the CSV data from the API
     response = requests.get(url)
