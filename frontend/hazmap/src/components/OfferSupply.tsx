179d36962a83d12b51dcd4b89eb44a1465ae1988
Nithika <nithika.karuna@gmail.com>
2025-11-09 14:02:24 -0500

    deal with merge conflicts for packages

--- frontend/hazmap/src/components/OfferSupply.tsx
+++ frontend/hazmap/src/components/OfferSupply.tsx
@@ -6,7 +6,7 @@ import { Input } from './ui/input';
 import { Label } from './ui/label';
 import { Textarea } from './ui/textarea';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
-import { toast } from 'sonner@2.0.3';
+import { toast } from 'sonner';
 import type { Page } from '../App';
 
 interface OfferSupplyProps {
